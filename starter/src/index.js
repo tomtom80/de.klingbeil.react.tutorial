3a8bbbabc7afee93356f7b89956c4aa0a54fc8f4
tomtom80 <tom.klingbeil@gmail.com>
2017-01-31 13:16:43 +0100

    lecture 13

--- starter/src/index.js
+++ starter/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyADpNC9ZFxrFouwziK2mjH649obhV3kc8Y';
+
 const App = () => {
   return <div>Hello World!</div>
 };
