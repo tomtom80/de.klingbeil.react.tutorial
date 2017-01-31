e5cbe82be182079e6cbb71888f6b52098d6761ab
tomtom80 <tom.klingbeil@gmail.com>
2017-01-31 13:29:12 +0100

    lecture 14

--- starter/src/index.js
+++ starter/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyADpNC9ZFxrFouwziK2mjH649obhV3kc8Y';
 
 const App = () => {
-  return <div>Hello World!</div>
+  return <div>
+    <SearchBar />
+  </div>
 };
 
 ReactDOM.render(
