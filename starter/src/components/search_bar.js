372b693ec1f526a7659c9c07ebe0fbb15784b4b8
tomtom80 <tom.klingbeil@gmail.com>
2017-01-31 13:52:05 +0100

    lecture 16

--- starter/src/components/search_bar.js
+++ starter/src/components/search_bar.js
@@ -1,8 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class SearchBar extends Component {
-    render(){
-        return <input />
+    render() {
+        return <input onChange={this.onInputChange} />
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
     }
 }
 
