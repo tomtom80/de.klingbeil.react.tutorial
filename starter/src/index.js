63c7bf15182c54cb76176661948d4c24a18654c5
tomtom80 <tom.klingbeil@gmail.com>
2017-02-01 00:03:47 +0100

    lecture 23

--- starter/src/index.js
+++ starter/src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyADpNC9ZFxrFouwziK2mjH649obhV3kc8Y';
 
-const App = () => {
-  return <div>
-    <SearchBar />
-  </div>
-};
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
+      this.setState({ videos })
+    });
+  }
+
+  render() {
+    return <div>
+      <SearchBar />
+    </div>
+  };
+}
 
 ReactDOM.render(
   <App />
