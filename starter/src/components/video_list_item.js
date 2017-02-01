712de459918e9886607e86791a8e8a539d0e8b08
tomtom80 <tom.klingbeil@gmail.com>
2017-02-01 21:40:17 +0100

    lecture 26

--- starter/src/components/video_list_item.js
+++ starter/src/components/video_list_item.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
+const VideoListItem = ({video}) => {
 
+    const imageURL = video.snippet.thumbnails.default.url;
     return (
-        <li>Video</li>
+        <li className="list-group-item">
+            <div className="media-left">
+                <img src={imageURL} className="media-object" />
+            </div>
+            <div className="media-body">
+                <div className="media-heading">{video.snippet.title}</div>
+            </div>
+        </li>
     )
 }
 
