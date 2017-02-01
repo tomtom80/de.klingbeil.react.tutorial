9f8571228625eaa7faafad092ceeebcdb05d3072
tomtom80 <tom.klingbeil@gmail.com>
2017-02-01 22:07:17 +0100

    lecture 28

--- starter/src/components/video_list.js
+++ starter/src/components/video_list.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import VideoListItem from './video_list_item';
+
+
 const VideoList = (props) => {
 
     const videoItems = props.videos.map((video) => {
-        return <VideoListItem key={video.etag} video={video} />
+        return (
+            <div>
+                <VideoListItem key={video.etag} video={video} />
+            </div>
+        )
     }
     )
 
