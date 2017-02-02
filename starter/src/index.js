ca8e5d253b49b993522ec2dc21978b71455fb798
tomtom80 <tom.klingbeil@gmail.com>
2017-02-02 14:24:43 +0100

    lecture 32

--- starter/src/index.js
+++ starter/src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
@@ -26,8 +27,9 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch = _.debounce( term => this.videoSearch(term), 300);
     return <div>
-      <SearchBar onSearchTermChanged={term => this.videoSearch(term)} />
+      <SearchBar onSearchTermChanged={videoSearch} />
       <VideoDetail video={this.state.selectedVideo} />
       <VideoList
         videos={this.state.videos}
