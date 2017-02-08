4906f5da8620437b9a6cef7842c048a5f8f67dd3
tomtom80 <tom.klingbeil@gmail.com>
2017-02-08 22:42:24 +0100

    lecture 53

--- forcast/src/containers/search_bar.js
+++ forcast/src/containers/search_bar.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+        this.setState({ term: event.target.value })
+    }
+
     render() {
         return (
             <form className="input-group">
-                <input />
+                <input
+                    className="form-control"
+                    placeholder="type in your city"
+                    value={this.state.term}
+                    onChange={this.onInputChange} />
                 <span className="input-group-btn">
-                    <input type="submit" className="btn btn-secondary" />
+                    <button type="submit" className="btn btn-secondary" >Submit</button>
                 </span>
             </form>
         )
