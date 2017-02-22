7497236c24d425c40cd76df53ebe2c4608ac8e73
tomtom80 <tom.klingbeil@gmail.com>
2017-02-22 22:25:14 +0100

    lecture 63

--- forcast/src/actions/index.js
+++ forcast/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '5cf52f52176b337b7f9dfda737302781';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
---
+++ forcast/src/containers/weather_list.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
+
+class WeatherList extends Component {
+
+    renderWeather(cityData) {
+        const name = cityData.city.name
+        const temp = cityData.list.map(weather => weather.main.temp);
+        return (
+            <tr key={name}>
+                <td >{name}</td>
+                <td >
+                    <Sparklines width={180} height={120} data={temp}>
+                        <SparklinesLine color="red" />
+                    </Sparklines>
+                </td>
+            </tr>
+
+        );
+    }
+
+    render() {
+        return (
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>City</th>
+                        <th>Temperature</th>
+                        <th>Pressure</th>
+                        <th>Humidity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.weather.map(this.renderWeather)}
+                </tbody>
+            </table>
+        )
+
+    }
+
+
+}
+
+function mapStateToProps({weather}) {
+    return { weather }
+}
+
+export default connect(mapStateToProps)(WeatherList);
