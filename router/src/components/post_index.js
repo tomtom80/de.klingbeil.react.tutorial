7579ea301d2ae6b110a367bf2dda540bd7ac101f
tomtom80 <tom.klingbeil@gmail.com>
2017-10-19 22:14:19 +0200

    react router 4

--- router/src/components/post_index.js
+++ router/src/components/post_index.js
@@ -1,13 +1,38 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions'
 
 class PostIndex extends Component {
+
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
+    renderPosts() {
+        return _.map(this.props.posts, post => {
+            return (
+                <li className="list-group-item" key={post.id} >
+                    {post.title}
+                </li>
+            );
+        });
+    }
+
     render() {
         return (
             <div>
-                Posts Index
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
             </div>
         )
     }
 }
 
-export default PostIndex;
+function mapStateToProps(state) {
+    return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostIndex);
--- router/src/index.js
+++ router/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
-import ReduxPromise  from 'redux-promise';
+import promise  from 'redux-promise';
 import reducers from './reducers';
 import PostIndex from './components/post_index'
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
