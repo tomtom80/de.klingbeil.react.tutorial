b5e7e2e47b8f786a2fef44cc227df465326cca43
tomtom80 <tom.klingbeil@gmail.com>
2017-10-19 20:59:30 +0200

    react router 2

--- router/src/index.js
+++ router/src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
+import { BrowserRouter, Route } from 'react-router-dom'
 import reducers from './reducers';
+import PostIndex from './components/post_index'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostIndex} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
